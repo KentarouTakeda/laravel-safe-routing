5134b21971067cb681497dd7a79b6cdedcff061f
武田 憲太郎 <takeda@youmind.jp>
2019-11-14 21:45:01 +0900

    schemaに default: false を指定するとapplyDefaultを行わない

--- tests/ApplyDefaultTest.php
+++ tests/ApplyDefaultTest.php
@@ -29,4 +29,29 @@ class ApplyDefaultTest extends TestCase
         $response = $this->get('/viewname');
         $response->assertViewHas('universe', 42);
     }
+
+    /** @test */
+    public function testResponseValidationNotApplyDefault() {
+        resolve(SafeRouting::class)->makeRoute([
+            'namespace' => __NAMESPACE__ . '\Controllers',
+            'routes' => [
+                'viewname' => [
+                    'controller' => 'SomeController@withReturn',
+                    'RET' => [
+                        'default' => false,
+                        'type' => 'object',
+                        'properties' => [
+                            'universe' => [
+                                'type' => "integer",
+                                'default' => 42
+                            ]
+                        ]
+                    ]
+                ],
+            ],
+        ]);
+
+        $response = $this->get('/viewname');
+        $response->assertViewMissing('universe');
+    }
 }
